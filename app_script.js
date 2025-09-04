c136231d5f30ad7e3fffd70af458dbac00c6c8d3
gaokerena <amiel.mathieu@etik.com>
2025-09-04 20:36:31 +0200

    Update app_script.js

--- app_script.js
+++ app_script.js
@@ -1,5 +1,6 @@
 const scriptURL = "https://script.google.com/macros/s/AKfycbxaYXlocGh2AfEbyi8KcPEeN4GNxRkXkUVwfxOZGuxQPBc48jatKK-ILhb_N4Kby8H9/exec";
 
+const spreadsheetPreviewURL = "https://docs.google.com/spreadsheets/d/14KMz5xjoJVF4UwlsG49kFtKoXCAp6hnZ8EL2DXPhtRs/preview";
 function showSection(contentHTML) {
   const container = document.getElementById("content");
   container.innerHTML = contentHTML;
@@ -52,10 +53,11 @@ function showOutput() {
 }
 
 function showParams() {
-  showSection(`
-    <h2>Params</h2>
-    <p>Here you can show configurable parameters or info.</p>
-  `);
+  const html = `
+    <h2>Params (Preview)</h2>
+    <iframe src="${spreadsheetPreviewURL}" style="width:100%; height:80vh; border:none;"></iframe>
+  `;
+  showSection(html);
 }
 
 // On page load, ensure PIN is valid
